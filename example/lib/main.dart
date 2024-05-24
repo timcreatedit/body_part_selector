cf2da169d53fc19291ba2a3ddd4e5e8ac29ec17a
Tim Lehmann <hi@timcreated.it>
2024-05-24 16:18:22 +0200

    build!: bump flutter version

---
+++ example/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:body_part_selector/body_part_selector.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Body Part Selector',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+      ),
+      home: const MyHomePage(title: 'Body Part Selector'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({required this.title, super.key});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  BodyParts _bodyParts = const BodyParts();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: BodyPartSelectorTurnable(
+          bodyParts: _bodyParts,
+          onSelectionUpdated: (p) => setState(() => _bodyParts = p),
+          labelData: const RotationStageLabelData(
+            front: 'Vorne',
+            left: 'Links',
+            right: 'Rechts',
+            back: 'Hinten',
+          ),
+        ),
+      ),
+    );
+  }
+}
