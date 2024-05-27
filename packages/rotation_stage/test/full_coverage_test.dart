51c225ecb871bb59429e30c99ffec7c855bd8316
Tim Lehmann <hi@timcreated.it>
2024-05-27 11:09:47 +0200

    test: added full_coverage

---
+++ packages/rotation_stage/test/full_coverage_test.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: unused_import
+import 'package:rotation_stage/rotation_stage.dart';
+import 'package:rotation_stage/src/model/rotation_stage_side.dart';
+import 'package:rotation_stage/src/rotation_stage_bar.dart';
+import 'package:rotation_stage/src/rotation_stage_content.dart';
+import 'package:rotation_stage/src/rotation_stage_controller.dart';
+import 'package:rotation_stage/src/rotation_stage_handle.dart';
+import 'package:rotation_stage/src/rotation_stage_labels.dart';
+
+void main() {}
---
+++ test/full_coverage_test.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: unused_import
+import 'package:body_part_selector/body_part_selector.dart';
+import 'package:body_part_selector/src/body_part_selector.dart';
+import 'package:body_part_selector/src/body_part_selector_turnable.dart';
+import 'package:body_part_selector/src/model/body_parts.dart';
+import 'package:body_part_selector/src/model/body_side.dart';
+import 'package:body_part_selector/src/service/svg_service.dart';
+
+void main() {}
